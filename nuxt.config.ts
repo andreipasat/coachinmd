97dcfa1bf1567fcb316852989536a29fa135cf46
pasatandrei <ap@blue10.com>
2025-01-06 17:12:49 +0200

    Add site url in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: { url: 'coachinmd.com' },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
