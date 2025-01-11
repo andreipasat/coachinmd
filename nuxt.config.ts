94379894a08640067c117cbe36fcbd9c49546a4b
pasatandrei <ap@blue10.com>
2025-01-11 16:35:21 +0200

    act112

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   site: { url: 'coachinmd.com' },
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2024-04-03',
   ssr: true,
   target: 'static',
   build: {
